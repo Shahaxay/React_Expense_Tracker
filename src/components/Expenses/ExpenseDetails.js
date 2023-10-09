f4336f7e0af1c7564a62f02839469d9a68a5af37
AkshayShah <shahaxay34@gmail.com>
2023-10-10 04:40:14 +0530

    manipulating the DOM - React States to update value on screen

--- src/components/Expenses/ExpenseDetails.js
+++ src/components/Expenses/ExpenseDetails.js
@@ -1,11 +1,21 @@
 import './ExpenseItem.css'
-import React from 'react';
+import React,{useState} from 'react';
 const ExpenseDetails = (props) => {
+    const [title,setTitle]=useState(props.title);
+    const [amount,setAmount]=useState(props.amount);
+    const changeTitleHandler=()=>{
+        setTitle("updated")
+    }
+    const changeAmountHandler=()=>{
+        setAmount('$100');
+    }
     return (
         <div className='expense-item__description'>
-            <h2>{props.title}</h2>
+            <h2>{title}</h2>
             <h2>{props.location}</h2>
-            <div className='expense-item__price'>{props.amount}</div>
+            <div className='expense-item__price'>{amount}</div>
+            <button onClick={changeTitleHandler}>Change Title</button>
+            <button onClick={changeAmountHandler}>Change Amount</button>
         </div>
     );
 }
