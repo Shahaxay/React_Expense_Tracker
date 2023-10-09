fb648db9167b13538297176acc7163736400cca7
Akshayshah47 <removeit61@gmail.com>
2023-10-09 13:37:01 +0530

    basic project setup is done

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <h2>ExpenseItems!</h2>
+            <p>Food Rs 10</p>
+            <p>Petrol Rs 100</p>
+            <p>Movies Rs 200</p>
+        </div>
+    );
+}
+
+export default ExpenseItem;
