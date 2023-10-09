fa0ed5028fbaf02f0b15a4d4d0b0594a319b8889
Akshayshah47 <removeit61@gmail.com>
2023-10-09 15:09:28 +0530

    styling and JS are used in JSX

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,17 @@
+import './ExpenseItem.css'
 function ExpenseItem() {
+    const expenseDate=new Date(2022,9,18);
+    const expenseTitle='Car Insurance';
+    const expenseAmount='Rs.10000';
+    const locationOfExpenditure='Banglore'
     return (
-        <div>
-            <h2>ExpenseItems!</h2>
-            <p>Food Rs 10</p>
-            <p>Petrol Rs 100</p>
-            <p>Movies Rs 200</p>
+        <div className="expense-item">
+            <div>{expenseDate.toDateString()}</div>
+            <div className="expense-item__description">
+                <h2>{expenseTitle}</h2>
+                <h2>{locationOfExpenditure}</h2>
+                <div className="expense-item__price">{expenseAmount}</div>
+            </div>
         </div>
     );
 }
