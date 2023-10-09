fb8bd5415a90cbcae82f0f22674c46e253b471d5
Akshayshah47 <removeit61@gmail.com>
2023-10-09 15:39:23 +0530

    making component reusable by passing data via props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,12 @@
 import './ExpenseItem.css'
-function ExpenseItem() {
-    const expenseDate=new Date(2022,9,18);
-    const expenseTitle='Car Insurance';
-    const expenseAmount='Rs.10000';
-    const locationOfExpenditure='Banglore'
+function ExpenseItem(props) {
     return (
         <div className="expense-item">
-            <div>{expenseDate.toDateString()}</div>
+            <div>{props.date.toDateString()}</div>
             <div className="expense-item__description">
-                <h2>{expenseTitle}</h2>
-                <h2>{locationOfExpenditure}</h2>
-                <div className="expense-item__price">{expenseAmount}</div>
+                <h2>{props.title}</h2>
+                <h2>{props.location}</h2>
+                <div className="expense-item__price">{props.amount}</div>
             </div>
         </div>
     );
