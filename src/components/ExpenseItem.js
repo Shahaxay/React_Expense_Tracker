18e432309e8f5b208bf1c32fb2712244365c8d03
AkshayShah <shahaxay34@gmail.com>
2023-10-09 16:32:30 +0530

    ExpenseItem component is splited into ExpenseDate and ExpenseDetails components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,14 @@
 import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate'
+import ExpenseDetails from './ExpenseDetails';
 function ExpenseItem(props) {
+
     return (
         <div className="expense-item">
-            <div>{props.date.toDateString()}</div>
-            <div className="expense-item__description">
-                <h2>{props.title}</h2>
-                <h2>{props.location}</h2>
-                <div className="expense-item__price">{props.amount}</div>
-            </div>
+            <ExpenseDate date={props.date} />
+            <ExpenseDetails amount={props.amount}
+                location={props.location}
+                title={props.title} />
         </div>
     );
 }
